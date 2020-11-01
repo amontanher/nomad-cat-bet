7a1e5372155c2e02ec9810b9c07b3cde423f7e24
Alessandra <alessandraamontanher@gmail.com>
2020-11-01 20:46:30 -0300

    Implements API endpoints (#2)
    * fishes list
    
    * implements bet
    
    * implements bet post

--- src/MainController.js
+++ src/MainController.js
@@ -1,5 +1,8 @@
 const {currentTemperatureByCity} = require('./api/weather')
 
+const Fish = require('../src/models/Fish');
+const Bet = require('../src/models/Bet');
+
 module.exports = {
   async getAllFishes(req, res) {
     const { city } = req.query;
@@ -7,29 +10,33 @@ module.exports = {
     const weather = await currentTemperatureByCity(city);
 
     let result;
-    if(weather.main.temp > 22){
-        result = {
-            body: JSON.stringify(
-            {
-                message: "peixes",
-            }),
-        };
+    if(weather.main.temp < 22){
+        const fishes = await Fish.find();
+        result = {fishes: fishes};
     }else{
-        result = {
-            body: JSON.stringify(
-            {
-                message: "sem peixes",
-            }),
-        };
+        result = {fishes: []};
     }
 
     return res.status(200).send(result);
   },
   async createBet(req, res) {
-    const { catName, fishId, totalRation } = req.body;
+    const { catName, fishId, ration } = req.body;
+
+    const bet = await Bet.create({
+        CatName: catName,
+        Fish: fishId,
+        Ration: ration
+    });
+
+    await bet.populate('fish').execPopulate();
     return res.status(204).send();
   },
   async getAllBets(req, res){
-      return res.status(200).send();
+    const { catName } = req.query;
+
+    const catBets = await Bet.find({CatName: catName});
+    result = {catBets: catBets};
+
+    return res.status(200).send(result);
   }
 };
