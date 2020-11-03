1e3608ed774d8ad4d9d4451d77e93f3cd9206f86
Alessandra <alessandraamontanher@gmail.com>
2020-11-03 18:07:01 -0300

    deploy adjustments

---
+++ handler/app.js
@@ -0,0 +1,18 @@
+const bodyParser = require('body-parser');
+const serverless = require('serverless-http');
+const express = require('express');
+const MainController = require("./../src/MainController");
+const mongoose = require('mongoose');
+const config = require('../config');
+const app = express();
+
+const uri = `mongodb+srv://${config.user}:${config.password}@clustermongo.yludg.mongodb.net/${config.dbName}?retryWrites=true&w=majority`;
+mongoose.connect(uri);
+
+app.use(bodyParser.json({ strict: false, limit: '10mb' }));
+
+app.get('/fishes', MainController.getAllFishes);
+app.get('/bet', MainController.getAllBets);
+app.post('/bet', MainController.createBet);
+
+module.exports.handler = serverless(app);
